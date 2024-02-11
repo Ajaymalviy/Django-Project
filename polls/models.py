b0e1f4cd119dbd722db6f6773d75d709be4da495
AjayMalviya <ajeymalviya143@gmail.com>
2024-02-11 23:16:34 +0530

    making changes into files of polls app where add new static files and css of a code and also make the changes into the file of view and index.html and also changes into the model

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 import datetime
 from django.utils import timezone
+from django.contrib import admin
+
+
+class Question(models.Model):
+    # ...
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Question(models.Model):
@@ -20,4 +33,6 @@ class Choice(models.Model):
         return self.choice_text
 
 
-
+def was_published_recently(self):
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,13 @@
+import datetime
+from unittest import TestCase
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
+from numpy import generic
 from .models import Question
 from django.template import loader
-
+from django.utils import timezone
+from django.urls import reverse
+ 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     template = loader.get_template("polls/index.html")
@@ -19,9 +24,7 @@ def index(request):
 #     def get_queryset(self):
 #         """Return the last five published questions."""
 #         return Question.objects.order_by("-pub_date")[:5]
-
-# Leave the rest of the views (detail, results, vote) unchangedeturn HttpResponse("Hello, world. You're at the polls index.")
-
+    
 def detail(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
@@ -29,9 +32,9 @@ def detail(request, question_id):
         raise Http404("Question does not exist")
     return render(request, "polls/detail.html", {"question": question})
 
-# def detail(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, "polls/detail.html", {"question": question})
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 # There’s also a get_list_or_404() function, which works just as get_object_or_404() – except using filter() instead of get(). 
@@ -75,3 +78,87 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+   #for testing  
+def get_queryset(self):
+    """
+    Return the last five published questions (not including those set to be
+    published in the future).Ye future k idea nhi rakhta privious h iske pass
+    """
+    return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+        :5
+    ]
+def create_question(question_text, days):
+    """
+    Create a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        """
+        If no questions exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
+
+    def test_past_question(self):
+        """
+        Questions with a pub_date in the past are displayed on the
+        index page.
+        """
+        question = create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(
+            response.context["latest_question_list"],
+            [question],
+        )
+
+    def test_future_question(self):
+        """
+        Questions with a pub_date in the future aren't displayed on
+        the index page.
+        """
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
+
+    def test_future_question_and_past_question(self):
+        """
+        Even if both past and future questions exist, only past questions
+        are displayed.
+        """
+        question = create_question(question_text="Past question.", days=-30)
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(
+            response.context["latest_question_list"],
+            [question],
+        )
+
+    def test_two_past_questions(self):
+        """
+        The questions index page may display multiple questions.
+        """
+        question1 = create_question(question_text="Past question 1.", days=-30)
+        question2 = create_question(question_text="Past question 2.", days=-5)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(
+            response.context["latest_question_list"],
+            [question2, question1],
+        )
+
+# class DetailView(generic.DetailView):
+#     ...
+
+#     def get_queryset(self):
+#         """
+#         Excludes any questions that aren't published yet.
+#         """
+#         return Question.objects.filter(pub_date__lte=timezone.now())
