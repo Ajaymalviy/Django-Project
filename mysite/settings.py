4319fb444f579e4b94c0f9720f559476dd12e12c
AjayMalviya <ajeymalviya143@gmail.com>
2024-02-22 15:26:59 +0530

    making new changes into file settings and urls ,in settings just change the default name of database now change it by Appointment_System of database which is in mongosh ,also adding new url of project urls

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'aapointment_system'
     'polls',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -75,10 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'Appointment_System',
+        'ENFORCE_SCHEMA': False,
     }
 }
 
