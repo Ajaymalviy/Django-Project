aeb99cf78db4a69e4d93d0be768b5e0a020e1670
AjayMalviya <ajeymalviya143@gmail.com>
2024-02-26 23:10:05 +0530

    appointment_system/urls.py
     again this is also the same files where i changes some function in views and its url for making migration

--- appointment_system/views.py
+++ appointment_system/views.py
@@ -1,4 +1,26 @@
 from django.http import HttpResponse
 
-def hello_world(request):
-    return HttpResponse("Hello, world!")
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+######------for listing documents of collection --------------
+from django.http import JsonResponse
+from appointment_system.models import Company  # Import your Django model
+from bson import json_util  # Import BSON to JSON converter
+
+def list_documents(request):
+    # Fetch all documents from the MongoDB collection
+    documents = Company.objects.all().values()
+
+    # Convert queryset to list of dictionaries
+    documents_list = list(documents)
+    print(documents_list)
+
+    # Serialize the data into JSON format
+    serialized_data = json_util.dumps(documents_list)
+    print(documents_list)
+
+    # Return JSON response
+    return JsonResponse(serialized_data, safe=False)
